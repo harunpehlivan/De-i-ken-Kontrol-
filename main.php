420d1ccad297ac6e28bb1473d1261d304e9a4160
HARUN PEHLİVAN <trcmnhp@hotmail.com>
2022-06-18 21:04:35 +0000

    Initial commit

---
+++ main.php
@@ -0,0 +1,21 @@
+
+<?php
+ 
+$test =5;
+if (is_integer($test))
+{
+    echo "test değişkeni integer<hr>";
+}
+else
+echo "test değişkeni integer değil<hr>";
+ 
+if (is_string($test))
+{
+    echo "test değişkeni string<hr>";
+}
+else
+echo "test değişkeni string değil<hr>";
+ 
+?>
+
+
